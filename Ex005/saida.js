2ea2bd9acdc103da420078f0315f8b3168c8ccf4
LocatelliBryann <bryannlocatelli@gmail.com>
2024-03-25 22:13:06 -0300

    Ex005 feito

---
+++ Ex005/saida.js
@@ -0,0 +1,13 @@
+// Supondo que a população de um país A seja da ordem de 80000 habitantes com uma taxa anual de crescimento de 3% 
+// e que a população de B seja 200000 habitantes com uma taxa de crescimento de 1.5%. Faça um programa que calcule e
+// escreva o número de anos necessários para que a população do país A ultrapasse ou iguale a população do país B, 
+// mantidas as taxas de crescimento
+
+const saida = require("./processamento")
+const entrada = require("./entrada")
+
+function mostrarSaida() {
+  console.log("Serao necessarios", saida.processamento().a," anos para a populacao do pais A igualar ou ultrapassar a polulacao do pais B.")
+  
+}
+module.exports = {mostrarSaida}
